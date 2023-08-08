f73e3dfb698741c3886392fb3f907da09034026e
Bogayan, Mark David L <mark.bogayan@gmanmi.com>
2023-08-08 11:50:52 +0800

    update password hashing

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import Password from 'node-php-password';
+import cryptJS from 'crypto-js';
+import Base64 from 'crypto-js/enc-base64';
 
 import UsersModel from '../users/users.model';
 
@@ -13,7 +16,7 @@ class AuthService {
 
         if (!user) throw new Conflict();
 
-        let match = await bcrypt.compare(password, user.password);
+        let match = await this.bcryptCompare(password, user.password)
 
         if (!match) throw new Conflict();
 
@@ -26,6 +29,34 @@ class AuthService {
         );
 
         return Promise.resolve({ token: token });
+    }
+
+    static async bcryptCompare(password: string, user_password: string): Promise<boolean> {
+        const isMatch = await bcrypt.compare(password, user_password);
+        return Promise.resolve(isMatch);
+    }
+
+    static async encryptPasswordToPHPHashing(password: string): Promise<string> {
+        const hash = cryptJS.SHA384(password);
+        const base = Base64.stringify(hash);
+
+        const encrypted = Password.hash(base, "PASSWORD_DEFAULT", { cost: 10 });
+
+        return Promise.resolve(encrypted);
+    }
+
+    static async verifiedPasswordPHPHashing(user_password: string, user_input: string): Promise<boolean> {
+        const hash = cryptJS.SHA384(user_input);
+        const base = Base64.stringify(hash);
+
+        const password = Password.needsRehash(user_password, "PASSWORD_DEFAULT", { cost: 10 });
+
+        if (password)
+            user_password = Password.hash(user_password, "PASSWORD_DEFAULT", { cost: 10 });
+
+        const compare = Password.verify(base, user_password);
+
+        return Promise.resolve(compare);
     }
 }
 
